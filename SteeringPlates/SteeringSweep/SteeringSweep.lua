bbdff4b1d614fd5031be0d777646ebb5306a419a
Christopher Chambers <christopher.chambers@mcgill.ca>
2022-07-15 17:55:17 -0700

    re-arranged the recording method to be in other_actions segmant

--- SteeringPlates/SteeringSweep/SteeringSweep.lua
+++ SteeringPlates/SteeringSweep/SteeringSweep.lua
@@ -16,14 +16,12 @@ local V1_voltage -- present voltage on V1
 local V3_voltage -- present voltage on V3
 local V1_sign -- polarity of V1_voltage
 local V3_sign -- polarity of V3_voltage
-local py_final -- final y position of ion
-local pz_final -- final z position of ion
 
 function segment.flym()
   sim_trajectory_image_control = 1 -- don't keep trajectories
 
   -- Step through all combinations of voltages V1 and V3.
-  for V1_pos = 1,math.ceil(math.abs((V1_start-V1_stop)/V1_step)) do
+  for V1_pos = 1,math.ceil(math.abs((V1_start-V1_stop)/V1_step))+1 do
     -- Update V1 voltage
     V1_voltage = V1_start + (V1_pos - 1) * V1_step
     if V1_voltage < 0
@@ -32,7 +30,7 @@ function segment.flym()
     else
       V1_sign = 'p'
     end
-    for V3_pos = 1,math.ceil(math.abs((V3_start-V3_stop)/V3_step)) do
+    for V3_pos = 1,math.ceil(math.abs((V3_start-V3_stop)/V3_step))+1 do
       -- Update V3 voltage
       V3_voltage = V3_start + (V3_pos - 1) * V3_step
       if V3_voltage < 0
@@ -53,8 +51,8 @@ end
 local first
 function segment.initialize_run()
   first = true
-  py_final = {}
-  pz_final = {}
+  file = io.open("data\\SteeringSweepData_V1_"..V1_sign..tostring(math.abs(V1_voltage)).."_V3_"..V3_sign..tostring(math.abs(V3_voltage))..".csv", 'w')
+  file:write("Generated from SteeringSweep.iob -- V1 = ",tostring(V1_voltage),"V V3 = ",tostring(V3_voltage),"V\nInitial Y pos (mm),Initial Z pos (mm),Final Y pos (mm), Final Z pos (mm)")
 end
 
 -- called multiple times per time-step to adjust voltages.
@@ -65,17 +63,22 @@ function segment.fast_adjust()
 
   -- called on every time-step for each particle in PA instance.
 function segment.other_actions()
-    -- Update the PE surface display on first time-step of run.
-    if first 
-    then 
-      first = false
-      sim_update_pe_surface = 1
-      file = io.open("data\\SteeringSweepData_V1_"..V1_sign..tostring(math.abs(V1_voltage)).."_V3_"..V3_sign..tostring(math.abs(V3_voltage))..".csv", 'w')
-      file:write("Generated from SteeringPlates.iob -- V1 = ",tostring(V1_voltage),"V V3 = ",tostring(V3_voltage),"V\nFinal Y pos (mm), Final Z pos (mm)")
-    end
+  -- Update the PE surface display on first time-step of run.
+  if first 
+  then 
+    first = false
+    sim_update_pe_surface = 1
   end
-
-function segment.terminate()
-    --record final positions of ion in units
+  -- These two if conditionals are checked each time-step on each ion in the run. Very useful for recording individual ion data at some point in the run.
+  -- Record initial position for each ion (once the new ion is created, the position is recorded at the first time-step)
+  if (ion_number ~= old_ion_num)
+  then
+    old_ion_num = ion_number
     file:write("\n",tostring(ion_py_mm),",",tostring(ion_pz_mm))
+  end
+  -- Record splat location for each ion (specifically leaving workbench)
+  if (ion_splat == -3)
+  then
+    file:write(",",tostring(ion_py_mm),",",tostring(ion_pz_mm))
+  end
 end
